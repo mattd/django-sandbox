ed52b277e9f561e5415c1e604669c5a0c0e00965
Matt Dawson <matthewtdawson@gmail.com>
2009-09-26 19:12:17 -0400

    initial commit

---
+++ sandbox/urls.py
@@ -0,0 +1,28 @@
+from os import path
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^sandbox/', include('sandbox.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
+
+# Use Django's static media server during developemnt.
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': path.abspath(path.dirname(__file___) +
+        '/static', 'show_indexes': True}),
+    )
